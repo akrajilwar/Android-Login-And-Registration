26023701d6226270b9f039cdb91ebefdabc71f1e
Akshay Raj <akrajilwar@users.noreply.github.com>
2020-04-28 08:30:20 +0530

    Update config.php

--- server_side_php/config.php
+++ server_side_php/config.php
@@ -1,44 +1,20 @@
-<?php 
+<?php
+    $username = "root";
+    $password = "";
+    $host = "localhost";
+    $dbname = "android_login";
 
-    $username = "root"; 
-    $password = "root"; 
-    $host = "localhost"; 
-    $dbname = "android_login"; 
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 
-    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'); 
- 
-    try { 
+    try {
+        $db = new PDO("mysql:host={$host};dbname={$dbname}", $username, $password);
+        // set the PDO error mode to exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $ex) {
+        die("Failed to connect to the database: " . $ex->getMessage());
+    }
 
-        $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options); 
+    // $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    } catch(PDOException $ex) { 
-
-        die("Failed to connect to the database: " . $ex->getMessage()); 
-    } 
- 
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
- 
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
- 
-    if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) { 
-        function undo_magic_quotes_gpc(&$array) { 
-            foreach($array as &$value) { 
-                if(is_array($value)) { 
-                    undo_magic_quotes_gpc($value); 
-                } 
-                else { 
-                    $value = stripslashes($value); 
-                } 
-            } 
-        } 
- 
-        undo_magic_quotes_gpc($_POST); 
-        undo_magic_quotes_gpc($_GET); 
-        undo_magic_quotes_gpc($_COOKIE); 
-    } 
- 
-    header('Content-Type: text/html; charset=utf-8'); 
- 
-    session_start(); 
- 
+    header('Content-Type: application/json; charset=utf-8');
 ?>
